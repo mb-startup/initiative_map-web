f5ca3d4d17a605d0a45030e7eb7d270251cc8b2f
DarkShaper1 <91323053+DarkShaper1@users.noreply.github.com>
2021-10-18 21:33:45 +0300

    Добавление новостей и личного кабинета. Лёгкие изменения в вёрстке

--- wp-content/themes/Test/index.php
+++ wp-content/themes/Test/index.php
@@ -55,53 +55,83 @@
 					wp_reset_postdata();
 
 				?>
+				<div class="news">
+					<h2 >Новости</h2>
+						<div class="row">
+							<?php 
+							$posts = get_posts( array(
+								'numberposts' => -1,
+								'category'    => 19,
+								'orderby'     => 'date',
+								'order'       => 'DESC',
+								'post_type'   => 'post',
+								'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+							) );
 
+					foreach( $posts as $post ){
+						setup_postdata($post);
+					    // формат вывода the_title() ...
+					    ?>
 
-			<!-- <div class="petition col-xl-4 col-md-6">
-			<div class="img-pet">
-					<img src="https://image.shutterstock.com/image-vector/no-image-vector-symbol-missing-260nw-1310632172.jpg" alt="">
+					    <div class="news-list col-4">
+							<div class="news-text">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								<p><?php the_content(); ?></p>
+							</div>
+						</div>
+					<?php
+						}
+						wp_reset_postdata();
+
+					?>
+
+						<!-- <div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+						<div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+						<div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+						<div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+						<div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+						<div class="news-list col-4">
+							<div class="news-text">
+								<h3>Title</h3>
+								<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab rem officia veniam eius molestiae deserunt asperiores!
+								</p>
+							</div>
+						</div>
+					 -->
+					</div>
 				</div>
-				<div class="text">
-					<h1>Заголовок</h1>
-					<h2>Описание</h2>
-				</div>
-				<div class="type">Тип: Здания</div>
-				<div class="address">Адрес: Блюхера 6а</div>
-				<div class="like">
-					<div class="img-like"><i class="fa fa-heart-o" aria-hidden="true" style="color:red;"></i></div>
-					<div class="number">160</div>
-				</div>
-			</div>
-			<div class="petition col-xl-4 col-md-6">
-			<div class="img-pet">
-					<img src="https://image.shutterstock.com/image-vector/no-image-vector-symbol-missing-260nw-1310632172.jpg" alt="">
-				</div>
-				<div class="text">
-					<h1>Заголовок</h1>
-					<h2>Описание</h2>
-				</div>
-				<div class="type">Тип: Здания</div>
-				<div class="address">Адрес: Блюхера 6а</div>
-				<div class="like">
-					<div class="img-like"><i class="fa fa-heart-o" aria-hidden="true" style="color:red;"></i></div>
-					<div class="number">160</div>
-				</div>
-			</div>
-			<div class="petition col-xl-4 col-md-6">
-			<div class="img-pet">
-					<img src="https://image.shutterstock.com/image-vector/no-image-vector-symbol-missing-260nw-1310632172.jpg" alt="">
-				</div>
-				<div class="text">
-					<h1>Заголовок</h1>
-					<h2>Описание</h2>
-				</div>
-				<div class="type">Тип: Здания</div>
-				<div class="address">Адрес: Блюхера 6а</div>
-				<div class="like">
-					<div class="img-like"><i class="fa fa-heart-o" aria-hidden="true" style="color:red;"></i></div>
-					<div class="number">160</div>
-				</div>
-			</div> -->
+				
+
 		</div>				
 	</div>
 <?php get_footer() ?>
---
+++ wp-content/themes/Test/page-profile.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+	Template name: Личный кабинет
+	*/
+?>
+
+<?php 
+	get_header();
+?>
+<div class="container">
+	<?php echo do_shortcode('[wp-recall]'); ?>
+</div>
+<?php 
+	get_footer();
+?>
